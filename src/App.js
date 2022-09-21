1412cc05d7b18cb83694937342d3e269a2d6bfc5
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-21 19:12:19 -0300

    return structure 2

--- src/App.js
+++ src/App.js
@@ -39,6 +39,16 @@ function App() {
           {/* job info */}
           <article className="job-info">
             <h3>{title}</h3>
+            <h4>{company}</h4>
+            <p className="job-date">{dates}</p>
+            {duties.map((duty, index) => {
+              return (
+                <div key={index} className="job-desc">
+                  <FaAngleDoubleRight className="job-icon" />
+                  <p>{duty}</p>
+                </div>
+              );
+            })}
           </article>
         </div>
       </div>
